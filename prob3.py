870f83969cc5e07d547cd9872caf63be4edefd77
Malcolm King <malcolm.elijah.king@gmail.com>
2024-02-12 15:40:59 -0600

    Added answer to prob3.py and added explanation

--- prob3.py
+++ prob3.py
@@ -1,4 +1,10 @@
 people = [("Alice", 30), ("Bob", 25), ("Charlie", 35)]
 # Use a lambda function as the sorting key
-sorted_people = sorted(people, key=lambda x: _)
+sorted_people = sorted(people, key=lambda x: x[1])
 print(sorted_people)
+
+'''
+Breakdown:
+Using x[1] on Line 3 essentially sorts the list of tuples by age.
+This is because x[1] refers to the SECOND element of each tuple (recall, list indicies start at 0)
+'''
